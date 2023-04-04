ef5a164f58a12636cdafbc924ffc93f552250185
awlsring <mattcanemail@gmail.com>
2023-04-03 23:14:16 -0700

    feat: provider methods on controller

--- packages/discord-common/src/service/serverboi.ts
+++ packages/discord-common/src/service/serverboi.ts
@@ -1,4 +1,4 @@
-import { GetServerCommand, ListServersCommand, ServerBoiClient, ServerSummary, TrackServerCommand, TrackServerInput } from "@serverboi/client";
+import { CreateProviderCommand, CreateProviderInput, DeleteProviderCommand, GetProviderCommand, GetServerCommand, ListProvidersCommand, ListServersCommand, ProviderSummary, ServerBoiClient, ServerSummary, TrackServerCommand, TrackServerInput, UntrackServerCommand } from "@serverboi/client";
 import { LRUCache } from "../cache/lru-cache";
 
 export class ServerBoiService {
@@ -17,7 +17,7 @@ export class ServerBoiService {
     }, 60 * 1000 /* 1 minute */);
   }
 
-  async getClientForUser(userId: string): Promise<ServerBoiClient> {
+  private async getClientForUser(userId: string): Promise<ServerBoiClient> {
     let client = this.clientCache.get(userId);
     if (!client) {
       console.log("ServerBoiService: creating new client for user", userId)
@@ -38,6 +38,36 @@ export class ServerBoiService {
       this.clientCache.set(userId, client);
     }
     return client;
+  }
+
+  async getProvider(user: string, name: string): Promise<ProviderSummary> {
+    const client = await this.getClientForUser(user);
+    const provider = await client.send(new GetProviderCommand({name}));
+    if (provider.summary) {
+      return provider.summary;
+    }
+    throw Error("ServerBoiService.getProvider: provider not found");
+  }
+
+  async listProviders(user: string): Promise<ProviderSummary[]> {
+    const client = await this.getClientForUser(user);
+    const providers = await client.send(new ListProvidersCommand({}));
+    return providers.summaries ?? [];
+  }
+
+  async createProvider(user: string, input: CreateProviderInput): Promise<ProviderSummary> {
+    const client = await this.getClientForUser(user);
+    const provider = await client.send(new CreateProviderCommand(input));
+    if (provider.summary) {
+      return provider.summary;
+    }
+    throw Error("ServerBoiService.createProvider: error making provider");
+  }
+
+  async deleteProvider(user: string, name: string): Promise<boolean> {
+    const client = await this.getClientForUser(user);
+    const response = await client.send(new DeleteProviderCommand({name}));
+    return response.success ?? false;
   }
 
   async getServer(user: string, id: string): Promise<ServerSummary> {
@@ -62,5 +92,11 @@ export class ServerBoiService {
       return server.summary;
     }
     throw Error("ServerBoiService.trackServer: server summary not found");
+  }
+
+  async untrackServer(user: string, id: string): Promise<boolean> {
+    const client = await this.getClientForUser(user);
+    const response = await client.send(new UntrackServerCommand({id}));
+    return response.success ?? false;
   }
 }
