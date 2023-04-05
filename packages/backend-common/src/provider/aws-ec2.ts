fef07955bb4e6c5dd24ca8e6b908a4f9a34e15e7
awlsring <mattcanemail@gmail.com>
2023-04-05 06:53:41 -0700

    feat: aws ec2 provider

---
+++ packages/backend-common/src/provider/aws-ec2.ts
@@ -0,0 +1,67 @@
+import { DescribeInstancesCommand, EC2Client, RebootInstancesCommand, StartInstancesCommand, StopInstancesCommand } from "@aws-sdk/client-ec2";
+import { ProviderAuthDto } from "../dto/provider-dto";
+import { Provider, ProviderServerData, State, Status } from "./provider";
+
+export interface AwsEc2ProviderOptions {
+  readonly region: string;
+}
+
+export interface AwsEc2ProviderServerData extends ProviderServerData {
+  readonly instanceId: string;
+}
+
+export class AwsEc2Provider implements Provider {
+  readonly client: EC2Client;
+
+  constructor(cfg: AwsEc2ProviderOptions, auth: ProviderAuthDto) {
+    if (!auth.secret) {
+      throw new Error("Missing secret on provider auth");
+    }
+
+    this.client = new EC2Client({ region: cfg.region, credentials: { accessKeyId: auth.key, secretAccessKey: auth.secret } });
+  }
+
+  async getServerStatus(serverData: AwsEc2ProviderServerData): Promise<Status> {
+    const instance = await this.client.send(new DescribeInstancesCommand({ InstanceIds: [serverData.instanceId] }))
+
+    if (!instance.Reservations || instance.Reservations.length === 0) {
+      return { state: State.UNKNOWN };
+    }
+
+    if (!instance.Reservations[0].Instances || instance.Reservations[0].Instances.length === 0) {
+      return { state: State.UNKNOWN };
+    }
+
+    if (!instance.Reservations[0].Instances[0].State) {
+      return { state: State.UNKNOWN };
+    }
+
+    const state = instance.Reservations[0].Instances[0].State.Name;
+
+    switch (state) {
+      case "running":
+        return { state: State.RUNNING };
+      case "pending":
+        return { state: State.STARTING };
+      case "stopped":
+        return { state: State.STOPPED };
+      case "stopping":
+      case "shutting-down":
+        return { state: State.STOPPING };
+      default:
+        return { state: State.UNKNOWN };
+    }
+  }
+
+  async startServer(serverData: AwsEc2ProviderServerData): Promise<void> {
+    await this.client.send(new StartInstancesCommand({ InstanceIds: [serverData.instanceId] }))
+  }
+
+  async stopServer(serverData: AwsEc2ProviderServerData): Promise<void> {
+    await this.client.send(new StopInstancesCommand({ InstanceIds: [serverData.instanceId] }))
+  }
+
+  async rebootServer(serverData: AwsEc2ProviderServerData): Promise<void> {
+    await this.client.send(new RebootInstancesCommand({ InstanceIds: [serverData.instanceId] }))
+  }
+}
