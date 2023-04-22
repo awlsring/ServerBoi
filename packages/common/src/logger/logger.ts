11a6196d33f7db8232166df783faaaa9502c6b7b
awlsring <mattcanemail@gmail.com>
2023-04-22 14:17:27 -0700

    fix: log to console

--- packages/common/src/logger/logger.ts
+++ packages/common/src/logger/logger.ts
@@ -6,11 +6,6 @@ export const logger = winston.createLogger({
   transports: [
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
     new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.Console({ format: winston.format.simple() })
   ],
 });
-
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
-}
